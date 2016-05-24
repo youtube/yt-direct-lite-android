7224efcb0417f3cb6ede2981a804fc9605e47285
Pranav Nair <pranavnair97@gmail.com>
2016-05-24 22:51:03 +0530

    Update Auth.java

--- app/src/main/java/com/google/ytdl/Auth.java
+++ app/src/main/java/com/google/ytdl/Auth.java
@@ -19,7 +19,7 @@ import com.google.api.services.youtube.YouTubeScopes;
 
 public class Auth {
     // Register an API key here: https://console.developers.google.com
-    public static final String KEY = "AIzaSyAJ6WxPuRxLoOCTxrpxx3DNm8To38z-Xyg";
+    public static final String KEY = "Enter your API key here";
 
     public static final String[] SCOPES = {Scopes.PROFILE, YouTubeScopes.YOUTUBE};
 }
